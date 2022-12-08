086f6e87c54a7ca29774fa3dca1ef4691956a26d
Javier Rojo <202109117@alu.comillas.edu>
2022-12-08 10:35:26 +0100

    main

--- Practica1_gitleaks/git_leaks.py
+++ Practica1_gitleaks/git_leaks.py
@@ -3,19 +3,23 @@ import re
 from github import Github
 
 REPO_DIR = './skale/skale-manager'
-info = ['key','keys','password','username','credentials','login','secret']
+sensible = ['key','keys','password','username','credentials','login','secret']
 
 def extract(repo_dir):
     repo = Repo(repo_dir) # path al repositorio con el que deseamos trabajar
-    commits = list(repo.iter_commits('develop'))
-    return commits
+    extracted = list(repo.iter_commits('develop'))
+    return extracted
+
+def search(_):
+    for word in sensible:
+        if re.search(word, _.message):
+            print('Commit: {} - {}'.format(_.hexsha, _.message))
 
 if __name__ == '__main__':
-    commits = extract(REPO_DIR)
-    for commit in commits:
-        for _ in info:
-            if re.search(_, commit.message, re.IGNORECASE):
-                print('Commit: {} - {}'.format(commit.hexsha, commit.message))
+    extracted = extract(REPO_DIR)
+    for _ in extracted:
+        search(_)
+        
                 
                 
 
